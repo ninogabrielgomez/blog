73ade8343bd636b845be22a92c748759f4819cb4
ninogabrielgomez <ngtgomez.student@ua.edu.ph>
2026-08-18 12:06:00 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I’m Niño. I’m a web development student at the University of the Assumption.</p>
+        <p>Hello, I’m Niño. I’m a Web Development student at the University of the Assumption. My hobbies include watching Western TV series, especially action and sitcoms. My favorite type of music is rhythm and blues (R&B), and one of my favorite comfort foods is kare-kare. In five years, I see myself becoming a Cloud Engineer, hopefully, or possibly a Network Engineer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
