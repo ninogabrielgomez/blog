d5584ca8fd4be71bfcd8d7f773f4da4b8d9411a8
ninogabrielgomez <ngtgomez.student@ua.edu.ph>
2026-08-18 12:06:19 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Niño Gabriel Gomez';
+const name = 'Niño Gabriel T. Gomez';
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
